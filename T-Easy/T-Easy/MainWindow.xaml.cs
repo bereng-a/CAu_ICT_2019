0a50073ca8f097e10a296f18f2f81fc51f4a1a80
My-Lan Aragon <mylan.aragon@gmail.com>
2019-05-15 14:44:36 +0900

    [UPDATE] Passing to mvvm

--- T-Easy/T-Easy/MainWindow.xaml.cs
+++ T-Easy/T-Easy/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using T_Easy.ViewModels;
 
 namespace T_Easy
 {
@@ -20,9 +8,12 @@ namespace T_Easy
     /// </summary>
     public partial class MainWindow : Window
     {
+        TravelViewModel _travelViewModel = new TravelViewModel();
+
         public MainWindow()
         {
             InitializeComponent();
+            base.DataContext = _travelViewModel;
         }
     }
 }
---
+++ T-Easy/T-Easy/ViewModels/TravelViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System.ComponentModel;
+using T_Easy.Models;
+
+namespace T_Easy.ViewModels
+{
+    public class TravelViewModel : INotifyPropertyChanged
+    {
+        #region Construction
+        public TravelViewModel()
+        {
+            _travel = new Travel { Name = null, SharingCode = null };
+        }
+        #endregion
+
+        #region Members
+        Travel _travel;
+        #endregion
+
+        #region Properties
+        public Travel Travel
+        {
+            get
+            {
+                return _travel;
+            }
+            set
+            {
+                _travel = value;
+            }
+        }
+
+        public string SharingCode
+        {
+            get
+            {
+                return _travel.SharingCode;
+            }
+            set
+            {
+                _travel.SharingCode = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _travel.Name;
+            }
+            set
+            {
+                _travel.Name = value;
+            }
+        }
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Methods
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            // take a copy to prevent thread issues
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
