fa9b9e73874996776426eec5d003c89999f2f51d
My-Lan Aragon <mylan.aragon@gmail.com>
2019-05-15 17:44:40 +0900

    [ADD] Login window and main window

---
+++ T-Easy/T-Easy/LoginWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using T_Easy.ViewModels;
+
+namespace T_Easy
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        TravelViewModel _travelViewModel = new TravelViewModel();
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+            base.DataContext = _travelViewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            App.Current.MainWindow = main;
+            this.Close();
+            main.Show();
+        }
+
+        private void Join_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            App.Current.MainWindow = main;
+            this.Close();
+            main.Show();
+        }
+    }
+}
--- T-Easy/T-Easy/MainWindow.xaml.cs
+++ T-Easy/T-Easy/MainWindow.xaml.cs
@@ -1,5 +1,16 @@
-﻿using System.Windows;
-using T_Easy.ViewModels;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace T_Easy
 {
@@ -8,12 +19,9 @@ namespace T_Easy
     /// </summary>
     public partial class MainWindow : Window
     {
-        TravelViewModel _travelViewModel = new TravelViewModel();
-
         public MainWindow()
         {
             InitializeComponent();
-            base.DataContext = _travelViewModel;
         }
     }
 }
