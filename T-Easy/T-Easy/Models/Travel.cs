b4e0fb6924e81f1116921807a61817b9da155357
My-Lan Aragon <mylan.aragon@gmail.com>
2019-05-15 14:17:06 +0900

    [ADD] Travel controller and model

---
+++ T-Easy/T-Easy/Models/Travel.cs
@@ -0,0 +1,11 @@
+﻿namespace T_Easy.Models
+{
+    /// <summary>
+    /// Class defining a travel
+    /// </summary>
+    public class Travel
+    {
+        public string SharingCode { get; set; }
+        public string Name { get; set; }
+    }
+}
