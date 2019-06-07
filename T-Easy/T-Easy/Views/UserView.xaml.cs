9f3489256e5eed7d8694b5185672348c347e1308
My-Lan Aragon <mylan.aragon@gmail.com>
2019-06-07 17:29:12 +0900

    [FIX] User fields

--- T-Easy/T-Easy/Views/UserView.xaml.cs
+++ T-Easy/T-Easy/Views/UserView.xaml.cs
@@ -22,12 +22,42 @@ namespace T_Easy.Views
         private void Add_Button_Click(object sender, RoutedEventArgs e)
         {
             _viewModel.CreateUser();
+            ClearFields();
             DestinationDialog.IsOpen = false;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
            _viewModel.DeleteUser(Int32.Parse(((Button)sender).Tag.ToString()));
+        }
+
+        private void NameField_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAddButton();
+        }
+
+        private void FamilyNameField_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAddButton();
+        }
+
+        private void UpdateAddButton()
+        {
+            if (NameField.Text.Length > 0 && FamilyNameField.Text.Length > 0)
+            {
+                AddButton.IsEnabled = true;
+            }
+        }
+
+        private void ClearFields()
+        {
+            NameField.Text = "";
+            FamilyNameField.Text = "";
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearFields();
         }
     }
 }
